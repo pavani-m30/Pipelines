a25d0c78f31832d37d3ffc70925ced20051497cc
pavani.m <mpavani3021@gmail.com>
2026-05-29 03:27:24 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,15 +1,6 @@
-
-m.pavani@LTPCHE032675099 MINGW64 ~/Desktop/fitness 2 (master)
-$ npm install
-npm error code ENOENT
-npm error syscall open
-npm error path C:\Users\m.pavani\Desktop\fitness 2\package.json
-npm error errno -4058
-npm error enoent Could not read package.json: Error: ENOENT: no such file or directory, open 'C:\Users\m.pavani\Desktop\fitness 2\package.json'
-npm error enoent This is related to npm not being able to find a file.
-npm error enoent
-npm error A complete log of this run can be found in: C:\Users\m.pavani\AppData\Local\npm-cache\_logs\2026-05-28T21_50_44_710Z-debug-0.log
-
-m.pavani@LTPCHE032675099 MINGW64 ~/Desktop/fitness 2 (master)
-$ node -v
-v24.13.1
+cd "C:/Users/m.pavani/Desktop/fitness 2/server"
+npm install
+# copy env example
+cp .env.example .env   # PowerShell: Copy-Item .env.example .env
+# edit .env to set MONGO_URI and JWT_SECRET (e.g. `notepad .env`)
+npm run dev
