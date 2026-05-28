89b1baf1cb61cfab0dbf6e160d86b78165dc464a
pavani.m <mpavani3021@gmail.com>
2026-05-29 03:29:08 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,3 +4,28 @@ npm install
 cp .env.example .env   # PowerShell: Copy-Item .env.example .env
 # edit .env to set MONGO_URI and JWT_SECRET (e.g. `notepad .env`)
 npm run dev
+# copy env example
+cp .env.example .env   # PowerShell: Copy-Item .env.example .env
+# edit .env to set MONGO_URI and JWT_SECRET (e.g. `notepad .env`)
+npm run dev
+bash: cd: C:/Users/m.pavani/Desktop/fitness 2/server: No such file or directory
+npm error code ENOENT
+npm error syscall open
+npm error path C:\Users\m.pavani\Desktop\fitness 2\package.json
+npm error errno -4058
+npm error enoent Could not read package.json: Error: ENOENT: no such file or directory, open 'C:\Users\m.pavani\Desktop\fitness 2\package.json'
+npm error enoent This is related to npm not being able to find a file.
+npm error enoent
+npm error A complete log of this run can be found in: C:\Users\m.pavani\AppData\Local\npm-cache\_logs\2026-05-28T21_58_19_627Z-debug-0.log
+cp: cannot stat '.env.example': No such file or directory
+npm error code ENOENT
+npm error syscall open
+npm error path C:\Users\m.pavani\Desktop\fitness 2\package.json
+npm error errno -4058
+npm error enoent Could not read package.json: Error: ENOENT: no such file or directory, open 'C:\Users\m.pavani\Desktop\fitness 2\package.json'
+npm error enoent This is related to npm not being able to find a file.
+npm error enoent
+npm error A complete log of this run can be found in: C:\Users\m.pavani\AppData\Local\npm-cache\_logs\2026-05-28T21_58_25_274Z-debug-0.log
+
+m.pavani@LTPCHE032675099 MINGW64 ~/Desktop/fitness 2 (master)
+$ 
